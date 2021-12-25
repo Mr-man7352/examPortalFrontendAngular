b702f40e91d574c8cb7a1b9fb2d5705f69b7613f
Manish Sahu <manissh7352@gmail.com>
2021-12-25 23:20:40 +0530

    signup is completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// components
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './pages/login/login.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
@@ -27,6 +31,9 @@ import { SignupComponent } from './pages/signup/signup.component';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  constructor(private userService: UserService) {}
 
-  constructor() { }
+  public user = {
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+  };
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  formSubmit() {
+    console.log(this.user);
+    if (this.user.username == '' || this.user.username == null) {
+      alert('user required');
+      return;
+    }
+    this.userService.addUser(this.user).subscribe(
+      (res) => {
+        console.log(res);
+        alert('user registered');
+      },
+      (err) => {
+        console.log(err);
+        alert('something went wrong');
+      }
+    );
   }
-
 }
