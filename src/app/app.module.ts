b75467a457ce18ce525b33b9658147b614d33f1d
Manish Sahu <manissh7352@gmail.com>
2021-12-26 02:15:49 +0530

    designed login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
 
 // components
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,7 @@ import { SignupComponent } from './pages/signup/signup.component';
     FooterComponent,
     LoginComponent,
     SignupComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,6 +39,7 @@ import { SignupComponent } from './pages/signup/signup.component';
     ReactiveFormsModule,
     HttpClientModule,
     MatSnackBarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  public user = {
+    username: '',
+    password: '',
+  };
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  formSubmit() {
+    console.log('hi');
   }
-
 }
