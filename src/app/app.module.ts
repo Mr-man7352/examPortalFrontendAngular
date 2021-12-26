7f6009d4e08aec2d27bcdd07bfdbf871527ad6f9
Manish Sahu <manissh7352@gmail.com>
2021-12-26 16:59:59 +0530

    nav bar completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 // components
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -40,6 +42,8 @@ import { HomeComponent } from './pages/home/home.component';
     HttpClientModule,
     MatSnackBarModule,
     MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
